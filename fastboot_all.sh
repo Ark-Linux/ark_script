4ee9f3bb7473f2b2e9d1e551992236892ffe4451
harry.chen <harry.chen@tymphany.com>
2019-12-16 15:08:10 +0800

    modify fastboot script, it can support windows and linux

--- fastboot_all.sh
+++ fastboot_all.sh
@@ -1,26 +1,28 @@
-#! /bin/bash
+#! /bin/sh
 
 set -eo pipefail
 
 readonly CRTDIR=$(pwd)
 readonly PKG_NAME="release_pkg"
 
-die() { echo $* >&2; exit 1; }
+die() { echo $* >&2; exec bash; }
 USAGE() {
 cat <<-EOF
 Usage:
 Fastboot flash a qcs40x
 
 $(basename $0) [-d SIDE] [-o EXTRA_FASTBOOT_FLASH_OPTIONS]
-* -b/--boot: (OPTIONAL) boot partition either a or b.
+* -b/--boot: boot partition either a or b.
+* -e/--environment: download environment either l(linux) or w(windows).
 * -o/--option: (OPTIONAL) space seperated string of fastboot flash arguments. 
   
 example:
-./$(basename $0) -b a
-./$(basename $0) -b a -o "--skip-reboot"
- 
+./$(basename $0) -b a -e l
+./$(basename $0) -b a -e l -o "--skip-reboot"
+
 EOF
-    exit 1
+    exec bash;
+#    exit 1
 }
 
 while [[ $# -gt 0 ]]; do
@@ -33,6 +35,10 @@ while [[ $# -gt 0 ]]; do
             OPTION=$2
             shift 2
             ;;
+        -e|--environment)
+            ENVIRONMENT=$2
+            shift 2
+            ;; 
         *)
             echo "Unknown option $1"
             USAGE
@@ -42,6 +48,11 @@ done
 
 if [[ ! "${BOOT}" =~ ^((a)|(b))$ ]]; then
     echo "ERROR: boot (${BOOT}) partition is not a or b"
+    USAGE
+fi
+
+if [[ ! "${ENVIRONMENT}" =~ ^((l)|(w))$ ]]; then
+    echo "ERROR: download environment is not l(linux) or w(windows)"
     USAGE
 fi
 
@@ -69,48 +80,57 @@ image_list=(
     usrdata.img   
 )
 
-
+set -x
 
 for image in ${image_list[@]}; do
     image_path=$(find ${CRTDIR} -name ${image} | head -1)
-    [[ -e ${image_path} ]] || die "${image_path} does not exist"
+    [[ -e ${image_path} ]] || die "${image} does not exist"
 done
 
-set -x
-
 COMMAND=`adb devices`
-#echo 'List of devices attached 7aee2f0 device' | sed 's/.*attached //g;s/ device.*//g'
 
-ADB_DEV_NUM=$(echo $COMMAND | grep -w "device" | sed 's/.*attached //g;s/ device.*//g')
+if [[ -z $(echo ${COMMAND} | grep -w "device" | sed 's/.*attached //g;s/ device.*//g') ]]; then
+    die "ERROR: list of devices is NULL"
+    exit
+fi
 
-[[ -n ${ADB_DEV_NUM} ]] || die "ERROR: list of devices is NULL"
-echo $ADB_DEV_NUM
+echo $(echo ${COMMAND} | grep -w "device" | sed 's/.*attached //g;s/ device.*//g')
 
 #enter fastboot mode
 adb reboot bootloader
-sudo fastboot devices
 
-sudo fastboot ${OPTION} flash bluetooth_${BOOT} ${PKG_NAME}/BTFM.bin
-sudo fastboot ${OPTION} flash cmnlib64_${BOOT} ${PKG_NAME}/cmnlib64.mbn
-sudo fastboot ${OPTION} flash cmnlib_${BOOT} ${PKG_NAME}/cmnlib.mbn
-sudo fastboot ${OPTION} flash devcfg_${BOOT} ${PKG_NAME}/devcfg.mbn
-sudo fastboot ${OPTION} flash dsp_${BOOT} ${PKG_NAME}/dspso.bin
-sudo fastboot ${OPTION} flash keymaster_${BOOT} ${PKG_NAME}/keymaster64.mbn
-sudo fastboot ${OPTION} flash logfs ${PKG_NAME}/logfs_ufs_8mb.bin
-sudo fastboot ${OPTION} flash modem_${BOOT} ${PKG_NAME}/NON-HLOS.bin
-sudo fastboot ${OPTION} flash pmic_${BOOT} ${PKG_NAME}/pmic.elf
-sudo fastboot ${OPTION} flash rpm_${BOOT} ${PKG_NAME}/rpm.mbn
-sudo fastboot ${OPTION} flash storsec ${PKG_NAME}/storsec.mbn
-sudo fastboot ${OPTION} flash tz_${BOOT} ${PKG_NAME}/tz.mbn
-sudo fastboot ${OPTION} flash uefisecapp_${BOOT} ${PKG_NAME}/uefi_sec.mbn
-sudo fastboot ${OPTION} flash xbl_${BOOT} ${PKG_NAME}/xbl.elf
+if [[ ${ENVIRONMENT} == "l" ]]; then
+    PERMISSIONS="sudo"
+    EXECUTABLE_PROGRAM=""
+else
+    PERMISSIONS=""
+    EXECUTABLE_PROGRAM=".exe"
+fi
 
-sudo fastboot ${OPTION} flash abl_${BOOT} ${PKG_NAME}/abl.elf
-sudo fastboot ${OPTION} flash boot_${BOOT}  ${PKG_NAME}/boot.img
-sudo fastboot ${OPTION} flash persist ${PKG_NAME}/persist.img
-sudo fastboot ${OPTION} flash system_${BOOT} ${PKG_NAME}/system.img
-sudo fastboot ${OPTION} flash systemrw ${PKG_NAME}/systemrw.img
-sudo fastboot ${OPTION} flash cache  ${PKG_NAME}/cache.img
-sudo fastboot ${OPTION} flash userdata ${PKG_NAME}/usrdata.img
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} devices
 
-sudo fastboot reboot
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash bluetooth_${BOOT} ${PKG_NAME}/BTFM.bin
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash cmnlib64_${BOOT} ${PKG_NAME}/cmnlib64.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash cmnlib_${BOOT} ${PKG_NAME}/cmnlib.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash devcfg_${BOOT} ${PKG_NAME}/devcfg.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash dsp_${BOOT} ${PKG_NAME}/dspso.bin
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash keymaster_${BOOT} ${PKG_NAME}/keymaster64.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash logfs ${PKG_NAME}/logfs_ufs_8mb.bin
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash modem_${BOOT} ${PKG_NAME}/NON-HLOS.bin
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash pmic_${BOOT} ${PKG_NAME}/pmic.elf
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash rpm_${BOOT} ${PKG_NAME}/rpm.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash storsec ${PKG_NAME}/storsec.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash tz_${BOOT} ${PKG_NAME}/tz.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash uefisecapp_${BOOT} ${PKG_NAME}/uefi_sec.mbn
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash xbl_${BOOT} ${PKG_NAME}/xbl.elf
+
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash abl_${BOOT} ${PKG_NAME}/abl.elf
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash boot_${BOOT}  ${PKG_NAME}/boot.img
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash persist ${PKG_NAME}/persist.img
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash system_${BOOT} ${PKG_NAME}/system.img
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash systemrw ${PKG_NAME}/systemrw.img
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash cache  ${PKG_NAME}/cache.img
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} ${OPTION} flash userdata ${PKG_NAME}/usrdata.img
+
+${PERMISSIONS} fastboot${EXECUTABLE_PROGRAM} reboot
+exec bash;
